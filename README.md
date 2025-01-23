f9c1ff14b841b461637c5cb0099dbe7cca4caf8f
mohazab2006 <mohazab2006@gmail.com>
2025-01-23 10:03:22 -0500

    Add initial website files

---
+++ README.md
@@ -0,0 +1,2 @@
+# EmpowerOrphans-Website-V1
+ Empower Orphans is a dedicated website for a fundraising foundation/club, designed to raise money for orphans around the world by supporting the Children's Aid Society of Ottawa. Built with HTML, CSS, and JavaScript, this responsive website connects with a broader audience to facilitate donations and help improve the lives of children in need.
---
+++ main.js
@@ -0,0 +1,82 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar_menu");
+const navLogo = document.querySelector("#navbar_logo");
+
+// Display Mobile Menu
+
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
+
+menu.addEventListener("click", mobileMenu);
+
+// Show Active menu
+
+const highlightMenu = () => {
+  const elem = document.querySelector(".highlight");
+  const homeMenu = document.querySelector("#home-page");
+  const aboutMenu = document.querySelector("#about-page");
+  const eventsMenu = document.querySelector("#events-page");
+  const charityMenu = document.querySelector("#charity-page");
+  let scrollPos = window.scrollY;
+  console.log(scrollPos);
+
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add("highlight");
+    homeMenu.classList.remove("highlight");
+    eventsMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    eventsMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    charityMenu.classList.remove("highlight");
+
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 3435) {
+    charityMenu.classList.add("highlight");
+    eventsMenu.classList.remove("highlight");
+    return;
+  }
+
+  if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove("highlight");
+  }
+};
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
+
+// Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector(".is-active");
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.remove("active");
+  }
+};
+
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
+
+let currentImageIndex = 0;
+
+function scrollGallery(direction) {
+  const gallery = document.querySelector(".gallery");
+  const images = document.querySelectorAll(".gallery-image-container");
+
+  currentImageIndex += direction;
+
+  if (currentImageIndex < 0) {
+    currentImageIndex = images.length - 1; // Loop to the last image
+  } else if (currentImageIndex >= images.length) {
+    currentImageIndex = 0; // Loop back to the first image
+  }
+
+  // Move the gallery to the current image
+  gallery.style.transform = `translateX(-${currentImageIndex * 100}%)`;
+}
